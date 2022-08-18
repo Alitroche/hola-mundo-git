cecf66358fbb9b1429cb11e0801e2c88acf59ad0
Alicia Troche <trochea906@gmail.com>
2022-08-18 14:30:33 -0400

    Se creo la funcion calcularEdad()

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ let nombre ="Juan Perez";
 let edad = 25;
 console.log("Hola " + nombre);
 console.log(nombre + " tiene "+ edad +" años");
+
+function calcularEdad(anioNacimiento){
+    const fechaActual = new Date();
+    const anioActual = anioActual.getFullYear();
+    const edad = anioActual - anioNacimiento;
+    return edad;
+}
